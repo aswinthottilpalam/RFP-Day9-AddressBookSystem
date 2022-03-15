fc81e6055e93206119c061430872af6485cf861f
aswinthottilpalam <aswinp264@gmail.com>
2022-03-15 17:58:31 +0530

    Edit contact details

--- Program.cs
+++ Program.cs
@@ -10,18 +10,21 @@ namespace AddressBookSystem
 
             while (true)
             {
-                Console.WriteLine("1. Add member to Contact list \n2.View Members in Contact List\n3.Exit");
+                Console.WriteLine("1. Add member to Contact list \n2.View Members in Contact List\n3.Edit Contact\n4.Exit");
                 Console.WriteLine("Enter an option:");
                 switch (Convert.ToInt32(Console.ReadLine()))
                 {
                     case 1:
-                        UC2AddNewContacts.AddMember();
+                        UC3EditDetails.AddMember();
                         break;
                     case 2:
-                        UC2AddNewContacts.ViewContacts();
+                        UC3EditDetails.ViewContacts();
+                        break;
+                    case 3:
+                        UC3EditDetails.EditDetails();
                         break;
 
-                    case 3:
+                    case 4:
                         // to exit from main method
                         Console.WriteLine("Exited");
                         return;
