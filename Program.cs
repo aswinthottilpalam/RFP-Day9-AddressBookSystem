968cb823877c575ed11b99dca5c3db8e268a527b
aswinthottilpalam <aswinp264@gmail.com>
2022-03-15 15:38:09 +0530

    Create address book and add details

--- Program.cs
+++ Program.cs
@@ -6,7 +6,30 @@ namespace AddressBookSystem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("********** Welcome to the  Address Book System  **********");
+
+            while (true)
+            {
+                //Console.WriteLine("*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*");
+                Console.WriteLine("1. Add member to Contact list \n2.View Members in Contact List\n3.Exit");
+                Console.WriteLine("Enter an option:");
+                switch (Convert.ToInt32(Console.ReadLine()))
+                {
+                    case 1:
+                        UC_1CreateAddressBook.AddMember();
+                        break;
+                    case 2:
+                        UC_1CreateAddressBook.ViewContacts();
+                        break;
+
+                    case 3:
+                        // to exit from main method
+                        Console.WriteLine("Exited");
+                        return;
+
+                }
+            }
+
         }
     }
 }
